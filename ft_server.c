c5c6d984181b95065546bf2570dee5836221bb4e
Hans Hazairi <hanshazairi@gmail.com>
2021-08-10 21:38:48 +0800

    chore: use sigaction struct

--- ft_server.c
+++ ft_server.c
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 12:20:54 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/08/08 18:25:53 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/08/10 21:37:09 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,25 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-static void	ft_test(int signum)
+static void	ft_sigaction(int sig, siginfo_t *siginfo, void *context)
 {
+	(void)siginfo;
+	(void)context;
 	ft_putnbr_fd(signum, 1);
 	ft_putchar_fd('\n', 1);
 }
 
 int	main(void)
 {
+	struct sigaction	s_sigaction;
+
 	ft_putstr_fd("Server PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
-	signal(SIGUSR1, ft_test);
-	signal(SIGUSR2, ft_test);
+	s_sigaction.sa_sigaction = ft_sigaction;
+	s_sigaction.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &s_sigaction, 0);
+	sigaction(SIGUSR2, &s_sigaction, 0);
 	while (1)
 		pause();
 	return (0);
