52a7dfd167adc62a1f3375ce69a569468fc2f7d6
Hans Hazairi <hanshazairi@gmail.com>
2021-11-24 16:10:41 +0800

    refactor: update code for clarity, uniformity & succintness

--- client.c
+++ client.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_client.c                                        :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 17:58:19 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/08/11 03:02:08 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/11/24 16:06:30 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-static void	ft_sigaction(int sig)
+static void	action(int sig)
 {
 	static int	received = 0;
 
@@ -29,7 +29,7 @@ static void	ft_sigaction(int sig)
 	}
 }
 
-static void	ft_kill(int pid, char *str)
+static void	mt_kill(int pid, char *str)
 {
 	int		i;
 	char	c;
@@ -57,19 +57,15 @@ static void	ft_kill(int pid, char *str)
 
 int	main(int argc, char **argv)
 {
-	struct sigaction	s_sigaction;
-
 	if (argc != 3 || !ft_strlen(argv[2]))
 		return (1);
 	ft_putstr_fd("Sent    : ", 1);
 	ft_putnbr_fd(ft_strlen(argv[2]), 1);
 	ft_putchar_fd('\n', 1);
 	ft_putstr_fd("Received: ", 1);
-	s_sigaction.sa_handler = ft_sigaction;
-	s_sigaction.sa_flags = SA_SIGINFO;
-	sigaction(SIGUSR1, &s_sigaction, 0);
-	sigaction(SIGUSR2, &s_sigaction, 0);
-	ft_kill(ft_atoi(argv[1]), argv[2]);
+	signal(SIGUSR1, action);
+	signal(SIGUSR2, action);
+	mt_kill(ft_atoi(argv[1]), argv[2]);
 	while (1)
 		pause();
 	return (0);
--- server.c
+++ server.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_server.c                                        :+:      :+:    :+:   */
+/*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 12:20:54 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/08/11 03:05:50 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/11/24 16:06:35 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <unistd.h>
 #include "libft/libft.h"
 
-static void	ft_sigaction(int sig, siginfo_t *siginfo, void *context)
+static void	action(int sig, siginfo_t *info, void *context)
 {
 	static int				i = 0;
 	static pid_t			client_pid = 0;
@@ -22,7 +22,7 @@ static void	ft_sigaction(int sig, siginfo_t *siginfo, void *context)
 
 	(void)context;
 	if (!client_pid)
-		client_pid = siginfo->si_pid;
+		client_pid = info->si_pid;
 	c |= (sig == SIGUSR2);
 	if (++i == 8)
 	{
@@ -48,7 +48,7 @@ int	main(void)
 	ft_putstr_fd("Server PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
-	s_sigaction.sa_sigaction = ft_sigaction;
+	s_sigaction.sa_sigaction = action;
 	s_sigaction.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &s_sigaction, 0);
 	sigaction(SIGUSR2, &s_sigaction, 0);
